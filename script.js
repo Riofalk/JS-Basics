11ac50c778adb6f96c9425ad39dc8dadad2897bd
Nikita Bogomazov <riofalk@gmail.com>
2022-08-24 17:59:29 +0300

    Task 1-5

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+const numberSum = (a,b) => {
+    return (a+b);
+}
+
+const lastThreeCharacters = (string) => {
+  let stringLength = string.length;
+  if (stringLength < 3) return
+  else return (string.slice(stringLength - 3, stringLength));
+}
+
+const numberIntoArray = (number) => {
+    let string = number.toString();
+    let numberArray = [];
+    if (typeof number != 'number' || number == '') return;
+    else {
+        for (let digit of string) {
+            numberArray.push(digit);
+        }
+    }
+    return numberArray;
+}
+
+const lastArrayElements = (array, n) => {
+    let arrayLength = array.length;
+    let newArray = array.slice(array.length - n, array.length);
+    return newArray;
+}
+
+const mergedArray = (firstArray, secondArray) => {
+    let mergedArray = firstArray.concat(secondArray);
+    let uniqueArray = [...new Set(mergedArray)];
+    uniqueArray =  uniqueArray.reverse();
+    return uniqueArray;
+}
+
